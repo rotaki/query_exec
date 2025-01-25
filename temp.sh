e2ac09e3e4a55b4db7a7bc34a77581eecbb44002
Charles Benello <31220609+cmbenello@users.noreply.github.com>
2025-01-25 12:45:19 -0600

    added flamegraph

--- temp.sh
+++ temp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Parameters
-BUFFER_POOL_SIZE=$1
-NUM_THREADS=$2
-WORKING_MEM=$3
+BUFFER_POOL_SIZE=150000
+NUM_THREADS=2
+WORKING_MEM=100
 
 # Set environment variables
 export NUM_THREADS=$NUM_THREADS
@@ -13,7 +13,8 @@ export WORKING_MEM=$WORKING_MEM
 rm -rf bp-dir-tpch-sf-1/0/??*
 
 # Run the benchmark with the specified parameters
-cargo run --release --bin benchmark_queries -- -q 100 -p bp-dir-tpch-sf-1 -n 1 -b $BUFFER_POOL_SIZE
+# cargo run --release --bin benchmark_queries -- -q 100 -p bp-dir-tpch-sf-1 -n 1 -b $BUFFER_POOL_SIZE
+sudo cargo flamegraph --bin benchmark_queries -- -q 100 -p bp-dir-tpch-sf-1 -n 1 -b $BUFFER_POOL_SIZE
 
 # Clean up after the run
 rm -rf bp-dir-tpch-sf-1/0/??*
